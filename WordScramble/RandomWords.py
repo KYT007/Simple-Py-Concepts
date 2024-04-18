2f6c5dc1f423c26e5f006264bf62b319a24d4af2
Kyle <yeakelkylet@gmail.com>
2024-04-17 20:19:55 -0400

    addding functions

--- WordScramble/RandomWords.py
+++ WordScramble/RandomWords.py
@@ -1,8 +1,11 @@
 from random_word import RandomWords
 import random
 
-def scramble_word(word):
+def scramble_word_level_1(word):
     word = list(word)
     random.shuffle(word)
     scrambled_word = ''.join(word)
     return scrambled_word
+
+def word_scramble_level_2(word):
+    pass
